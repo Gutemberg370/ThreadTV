7650ae297d7f3546f14acbab7af08cbc74274cc2
Gutemberg370 <gutembergmaia370@gmail.com>
2021-10-07 23:29:35 -0300

    fazendo upload de um projeto

---
+++ src/Thread_TV.java
@@ -0,0 +1,179 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.concurrent.Semaphore;
+
+class tvThread  {
+    public static Semaphore mutex = new Semaphore(1);
+    public static Semaphore tv = new Semaphore(1);
+    public static int quant_assistindo = 0;
+    public static int canal_atual = 0;
+    public static int n_canais;
+    public static int[] requisicao_canal;
+    public static Semaphore[] quant_requisicoes;
+    public static ArrayList<Hospede> hospedes  = new ArrayList<Hospede>();
+    public static View gui;
+
+    static class Hospede extends Thread {
+        private String id;
+        private int canal_favorito;
+        private int tempo_assistir;
+        private int tempo_descansar;
+        private int position; // Id interno para fins de interface.
+
+        public Hospede (String id, int canal_favorito, int tempo_assistir, int tempo_descansar) {
+            this.id = id;
+            this.canal_favorito = canal_favorito;
+            this.tempo_assistir = tempo_assistir;
+            this.tempo_descansar = tempo_descansar;
+            this.position = hospedes.size();
+        }
+
+        void mudar_canal() {
+            try {
+				mutex.acquire();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+            int fav_canal = this.canal_favorito - 1; // Para acesso na array de requisiï¿½ï¿½es
+            if (canal_atual != this.canal_favorito) {
+                if (requisicao_canal[fav_canal] == 0) {
+                    requisicao_canal[fav_canal] += 1;
+                    mutex.release();
+                    try {
+						tv.acquire();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                    
+                    try {
+        				mutex.acquire();
+        			} catch (InterruptedException e1) {
+        				e1.printStackTrace();
+        			}
+                    canal_atual = this.canal_favorito;
+                    gui.setChannelLabel(canal_atual);
+                    quant_requisicoes[fav_canal].release(requisicao_canal[fav_canal] - 1);
+                    requisicao_canal[fav_canal] = 0;
+                }
+                else {
+                    requisicao_canal[fav_canal] += 1;
+                    mutex.release();
+                    try {
+						quant_requisicoes[fav_canal].acquire();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                    try {
+						mutex.acquire();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                }
+            }
+            mutex.release();
+        }
+
+        void simula_assistir(int tempo) {
+        	gui.addLog(this.id + " está assistindo.");
+            long time = System.currentTimeMillis();
+            while (System.currentTimeMillis() - time < this.tempo_assistir * 1000) {
+            	long time2 = System.currentTimeMillis();
+            	while (System.currentTimeMillis() - time2 < 300) {};
+            	gui.setImage(this.position, "/assets/assistindo1.png");
+            	while (System.currentTimeMillis() - time2 < 600) {};
+            	gui.setImage(this.position, "/assets/assistindo2.png");
+            };
+            int final_time = (int)((System.currentTimeMillis() - time)/1000);
+            gui.addLog(String.format("%s assitiu TV por %d segundos.", this.id, final_time));
+        }
+
+        void simula_descansar(int tempo) {
+            gui.addLog(String.format("%s está descansando.", this.id));
+            long time = System.currentTimeMillis();
+            while (System.currentTimeMillis() - time < this.tempo_descansar * 1000) {
+            	long time2 = System.currentTimeMillis();
+            	while (System.currentTimeMillis() - time2 < 300) {};
+            	gui.setImage(this.position, "/assets/dormindo2.png");
+            	while (System.currentTimeMillis() - time2 < 600) {};
+            	gui.setImage(this.position, "/assets/dormindo1.png");
+            };
+            int final_time = (int)((System.currentTimeMillis() - time)/1000);
+            gui.addLog(String.format("%s descansou por %d segundos.", this.id, final_time));
+        }
+
+        void assistir() {
+            mudar_canal();
+            quant_assistindo += 1;
+            simula_assistir(this.tempo_assistir);
+            try {
+				mutex.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+            quant_assistindo -= 1;
+            if (quant_assistindo == 0 && tv.availablePermits() == 0) {
+                tv.release();
+                canal_atual = 0;
+                gui.setChannelLabel(canal_atual);
+            }
+            mutex.release();
+        }
+
+
+        void descansar(){
+            simula_descansar(this.tempo_descansar);
+        }
+
+        public void run() {
+            while (true) {
+                this.assistir();
+                this.descansar();
+            }
+        }
+    }
+
+
+    static void init_reqs() {
+        quant_requisicoes =  new Semaphore[n_canais];
+        requisicao_canal = new int[n_canais];
+        for(int i = 0; i < n_canais; i++) {
+            quant_requisicoes[i] = new Semaphore(0);
+            requisicao_canal[i] = 0;
+        }
+    }
+
+
+
+    public static void criaHospede() {
+        String identificador = gui.campoId.getText();
+        int canalFavorito = Integer.parseInt(gui.campoCanalFavorito.getText());
+        int tempoAssistindo = Integer.parseInt(gui.campoTempoAssistindo.getText());
+        int tempoDormindo = Integer.parseInt(gui.campoTempoDormindo.getText());
+        Hospede hospede = new Hospede(identificador, canalFavorito, tempoAssistindo, tempoDormindo);
+        hospede.setPriority(1);
+        hospede.start();
+        hospedes.add(hospede);
+    }
+    
+    public static void main(String[] args) {
+        gui = new View();
+        int resposta = gui.mostraFormularioNumeroDeCanais();
+        if (resposta == -1) return;
+        n_canais = resposta;
+        gui.setLayout(null);
+        gui.setUpComponents();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setSize(1000, 1000);
+        gui.setLocationRelativeTo( null );
+        gui.setResizable(false);
+
+        init_reqs();
+
+    }
+
+}
+
+
+
---
+++ src/View.java
@@ -0,0 +1,301 @@
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+/*
+    Frame - janela em que a view se encontra
+    Panel - containers que s√£o colocados dentro da view a fim de organizar os componentes
+    JLabel - apresentar uma label na tela
+    *OBS: Para mostrar uma imagem, √© necess√°rio criar uma label e adicionar uma ImageIcon a ela, caso n√£o
+    queira mostrar a label √© s√≥ deixar o texto em branco: " "
+
+ */
+public class View extends JFrame implements ActionListener {
+    private JLabel labelIconeTV;
+    private JPanel panel;
+    private JFrame logFrame;
+    private JScrollPane logScrollPane;
+    private JTextArea logTextArea;
+    private ArrayList<JPanel> cards  = new ArrayList<JPanel>();
+    private ArrayList<JLabel> hospedesTempoDormindo  = new ArrayList<JLabel>();
+    private ArrayList<JLabel> hospedesTempoAssistindo  = new ArrayList<JLabel>();
+    private ArrayList<JLabel> hospedesCanalFavorito  = new ArrayList<JLabel>();
+    private ArrayList<JLabel> hospedesId  = new ArrayList<JLabel>();
+    private ArrayList<JLabel> hospedesLabels  = new ArrayList<JLabel>();
+    private JButton botaoInserirHospede;
+    public JLabel labelTextoCanal;
+    public JTextField campoTempoDormindo;
+    public JTextField campoTempoAssistindo;
+    public JTextField campoCanalFavorito;
+    public JTextField fieldNumberOfChannels;
+    public JTextField campoId;
+    private int contaHospedes;
+
+    private ImageIcon resizeImage(ImageIcon icone, int width, int height) {
+        Image imagem = icone.getImage();
+        Image novoIconeTV = imagem.getScaledInstance(width, height,  Image.SCALE_SMOOTH);
+        icone = new ImageIcon(novoIconeTV);
+        return icone;
+    }
+    
+    public void setUpLog() {
+        logTextArea = new JTextArea();
+       
+        logFrame = new JFrame("LOG DAS THREADS");
+        logFrame.setLayout(null);
+        logFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        logFrame.setVisible(true);
+        logFrame.setBounds(0, 0, 400, 600);
+        logFrame.setResizable(true);
+  
+        logTextArea.setBounds(50, 50, 300, 300);
+        logTextArea.setEditable(false);
+        DefaultCaret caret = (DefaultCaret) logTextArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        
+        logScrollPane = new JScrollPane(logTextArea);
+        logScrollPane.setBounds(50, 50, 300, 500);
+        logFrame.add(logScrollPane);
+    }
+    
+    public void addLog(String text) {
+    	logTextArea.append(" - " + text + "\n");
+    }
+
+    public void setUpComponents() {
+        // panel que mais externo responsavel por configurar o layout dos cards dos hospedes
+        panel = new JPanel();
+        this.setUpLog();
+        panel.setBounds(50, 200, 900, 550);
+        panel.setLayout(new GridLayout(2,5,32,32));
+        add(panel); // adding panel into the frame
+  
+        configuraImagemTV();
+        configuraLabelCanal();
+        configuraCards();
+        configuraBotaoInserirHospede();
+        labelIconeTV.add(labelTextoCanal);
+    }
+
+    public void configuraImagemTV() {
+        ImageIcon iconeTV = new ImageIcon(getClass().getResource("/assets/tv.png"));
+        iconeTV = resizeImage(iconeTV,150,140);
+        labelIconeTV = new JLabel();
+        labelIconeTV.setText("");
+        labelIconeTV.setBounds(450,-10,200,200);
+        labelIconeTV.setIcon(iconeTV);
+        add(labelIconeTV);
+    }
+
+    public void configuraLabelCanal() {
+        labelTextoCanal = new JLabel("5", SwingConstants.CENTER);
+        labelTextoCanal.setLayout(null);
+        labelTextoCanal.setBounds(33,100,50,30);
+        labelTextoCanal.setFont(labelTextoCanal.getFont().deriveFont(28f));
+        labelTextoCanal.setVisible(false);
+        add(labelTextoCanal);
+    }
+
+    public void configuraCards() {
+        for (int i = 0; i<10; i++) { // cria todos os cards da tela
+            JPanel cardPanel = new JPanel();
+            cardPanel.setLayout(null);
+            cardPanel.setSize(101, 120);
+            cardPanel.setBackground(new Color(0xD4D2D2));
+            cardPanel.setName(Integer.toString(i + 1));
+            cardPanel.setVisible(true);
+            cardPanel.add(configuraImagemHospedeDormindo());
+            cardPanel.add(configuraStackDeInformacoesDoHospede()); // adiciona a stack que contem canal preferido, tempo assist, etc ao cardPanel
+            cardPanel.setVisible(false); // deixa os cards invisiveis, quando o usuario digitar um novo hospede ele fica visivel
+            cards.add(cardPanel); // adiciona o card em um array para que possamos acessa-lo futuramente
+            panel.add(cardPanel); // adiciona o cardPanel no panel que foi criado anteriormente na funcao setUpComponents() para organizar o layout
+        }
+    }
+
+    // esse i serve para que depois seja possivel acessar o JLabel inserido no array, em que cada posicao corresponde a um hospede
+    // essa funcao apenas retorna a imagem da pessoa dormindo ao cardPanel
+    public JLabel configuraImagemHospedeDormindo() {
+        JLabel labelIconePessoaDormindo = new JLabel();
+        ImageIcon iconePessoaDormindo = new ImageIcon(getClass().getResource("/assets/dormindo2.png"));
+        iconePessoaDormindo = resizeImage(iconePessoaDormindo, 94,92);
+        labelIconePessoaDormindo.setText("");
+        labelIconePessoaDormindo.setBounds(44,7,94,92);
+        labelIconePessoaDormindo.setIcon(iconePessoaDormindo);
+        hospedesLabels.add(labelIconePessoaDormindo);
+        return labelIconePessoaDormindo;
+    }
+    
+    public void setImage(int position, String source) {
+        ImageIcon icone = new ImageIcon(getClass().getResource(source));
+        icone = resizeImage(icone, 94, 92);
+    	hospedesLabels.get(position).setIcon(icone);
+    }
+
+    public void setChannelLabel(int channel) {
+    	this.labelTextoCanal.setText(String.valueOf(channel));
+    	this.labelTextoCanal.setVisible(true);
+    	this.labelTextoCanal.repaint();
+    }
+
+    // monta a stack de informacao do card
+    public JPanel configuraStackDeInformacoesDoHospede() {
+        JPanel panelStackInfo = new JPanel();
+        panelStackInfo.setBounds(40, 105, 100, 140);
+        panelStackInfo.setLayout(new GridLayout(4,2,21,10)); // configura o layout da informacao do hospede em 4 linhas e 2 colunas, espaco horizontal de 21 e vertical de 10
+        panelStackInfo.setBackground(new Color(0xD4D2D2));
+
+        // cria os icones
+        ImageIcon iconeId = new ImageIcon(getClass().getResource("/assets/id-pass.png"));
+        iconeId = resizeImage(iconeId, 27,24);
+
+        ImageIcon iconeTempoAssistindo = new ImageIcon(getClass().getResource("/assets/tvGuest.png"));
+        iconeTempoAssistindo = resizeImage(iconeTempoAssistindo, 27,24);
+
+        ImageIcon iconeControleRemoto = new ImageIcon(getClass().getResource("/assets/remoteControl.png"));
+        iconeControleRemoto = resizeImage(iconeControleRemoto, 20,30);
+
+        ImageIcon iconeTempoDormindo = new ImageIcon(getClass().getResource("/assets/sleepingIcon.png"));
+        iconeTempoDormindo = resizeImage(iconeTempoDormindo, 28,28);
+
+        // adiciona os icones a label para que possam ser mostrados na tela, labels apenas para apresentar as imagens
+        JLabel labelIconeId = new JLabel();
+        labelIconeId.setText("");
+        labelIconeId.setHorizontalTextPosition(JLabel.CENTER);
+        labelIconeId.setIcon(iconeId);
+
+        JLabel labelIconeTempoAssistindo = new JLabel();
+        labelIconeTempoAssistindo.setText("");
+        labelIconeTempoAssistindo.setHorizontalTextPosition(JLabel.CENTER);
+        labelIconeTempoAssistindo.setIcon(iconeTempoAssistindo);
+
+        JLabel labelIconeTempoDormindo = new JLabel();
+        labelIconeTempoDormindo.setText("");
+        labelIconeTempoDormindo.setHorizontalTextPosition(JLabel.CENTER);
+        labelIconeTempoDormindo.setIcon(iconeTempoDormindo);
+
+        JLabel labelIconeControleRemoto = new JLabel();
+        labelIconeControleRemoto.setText("");
+        labelIconeControleRemoto.setHorizontalTextPosition(JLabel.CENTER);
+        labelIconeControleRemoto.setIcon(iconeControleRemoto);
+
+        // configura labels que mostram o texto relacionado a cada icone
+        JLabel labelTextoId = new JLabel();
+        labelTextoId.setText("2");
+        labelTextoId.setFont(labelTextoId.getFont().deriveFont(18f));
+        hospedesId.add(labelTextoId);
+
+        JLabel labelTextoCanal = new JLabel();
+        labelTextoCanal.setText("2");
+        labelTextoCanal.setFont(labelTextoCanal.getFont().deriveFont(18f));
+        hospedesCanalFavorito.add(labelTextoCanal);
+
+        JLabel labelTextoTempoAssistindo = new JLabel();
+        labelTextoTempoAssistindo.setText("25");
+        labelTextoTempoAssistindo.setFont(labelTextoTempoAssistindo.getFont().deriveFont(18f));
+        hospedesTempoAssistindo.add(labelTextoTempoAssistindo);
+
+        JLabel labelTextoTempoDormindo = new JLabel();
+        labelTextoTempoDormindo.setText("30");
+        labelTextoTempoDormindo.setFont(labelTextoTempoDormindo.getFont().deriveFont(18f));
+        hospedesTempoDormindo.add(labelTextoTempoDormindo);
+
+        // adiciona os icones e labels ao panel stack
+        panelStackInfo.add(labelIconeId);
+        panelStackInfo.add(labelTextoId);
+        panelStackInfo.add(labelIconeControleRemoto);
+        panelStackInfo.add(labelTextoCanal);
+        panelStackInfo.add(labelIconeTempoAssistindo);
+        panelStackInfo.add(labelTextoTempoAssistindo);
+        panelStackInfo.add(labelIconeTempoDormindo);
+        panelStackInfo.add(labelTextoTempoDormindo);
+        return panelStackInfo;
+    }
+
+    public void configuraBotaoInserirHospede(){
+        botaoInserirHospede = new JButton();
+        botaoInserirHospede.setBounds(400, 650, 200, 50);
+        botaoInserirHospede.setText("Inserir HÛspede");
+        botaoInserirHospede.addActionListener(this);
+        add(botaoInserirHospede);
+    }
+
+    // acao do botao de inserir hospede
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == botaoInserirHospede){  // se quem enviou a acao foi o insertGuestButton
+            if (contaHospedes<10){
+                mostraFormulario(); // mostra o formulario em que o usuario pode cadastrar os dados do hospede
+                mostraDadosObtidosPeloFormularioNoCard(); // mostra o card com as informacoes que o usuario inseriu
+            } else { // caso o usuario tente cadastrar mais de 10 hospedes sera exibida uma mensagem dizendo que ele ja atingiu o limite maximo de hospedes.
+                mostraMensagemDeErro();
+            }
+        }
+    }
+
+    // mensagem de erro quando o usuario tenta cadastrar mais de 10 hospedes
+    public void mostraMensagemDeErro() {
+        JOptionPane.showMessageDialog(null,"N˙mero m·ximo de hÛspedes atingido.",
+                "Erro ao cadastrar HÛspede", JOptionPane.PLAIN_MESSAGE );
+    }
+
+    // mostra o formulario em que o usuario pode cadastrar os dados do hospede
+    public void mostraFormulario() {
+        contaHospedes++; // contagem para nao deixar o usuario cadastrar mais que 10 usuarios
+        campoId = new JTextField();
+        campoTempoDormindo = new JTextField();
+        campoTempoAssistindo = new JTextField();
+        campoCanalFavorito = new JTextField();
+        Object[] fields = {
+                "id", campoId,
+                "Canal Favorito", campoCanalFavorito,
+                "Tempo assistindo", campoTempoAssistindo,
+                "Tempo dormindo", campoTempoDormindo,
+        };
+        JOptionPane.showConfirmDialog(null, fields,"Informacıes do hÛspede", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        tvThread.criaHospede();
+    }
+
+    // apresenta o formulario para o usuario adicionar o numero de canais
+    public int mostraFormularioNumeroDeCanais() {
+    	final int BAD_INPUT = -2;
+    	int channel = 0;
+    	do {
+    		fieldNumberOfChannels = new JTextField();
+    		Object[] fields = {
+    				"N˙mero de canais", fieldNumberOfChannels,
+    		};
+    		int botao = JOptionPane.showConfirmDialog(null, fields,"Quantidade de canais", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+    		if (botao == JOptionPane.OK_OPTION) {
+    			String channelStr = fieldNumberOfChannels.getText();
+    			channelStr = channelStr.replaceAll("\\s","");
+    			try {
+    				channel = Integer.parseInt(channelStr);
+    				if (channel < 1) channel = BAD_INPUT;
+    			} catch (NumberFormatException e) {
+    				channel = BAD_INPUT;
+    			}
+    		} else if (botao == JOptionPane.CANCEL_OPTION || botao == JOptionPane.CLOSED_OPTION) {
+    			channel = -1;
+    		}
+    	} while (channel == BAD_INPUT);
+        return channel;
+    }
+
+    // mostra o card na tela com todas as informacoes que foram inseridas pelo usuario
+    public void mostraDadosObtidosPeloFormularioNoCard() {
+        JPanel cardHospede = cards.get(contaHospedes-1);
+        cardHospede.setVisible(true);
+        JLabel canalHospede = hospedesCanalFavorito.get(contaHospedes-1);
+        canalHospede.setText(campoCanalFavorito.getText());
+        JLabel tempoDormindo = hospedesTempoDormindo.get(contaHospedes-1);
+        tempoDormindo.setText(campoTempoDormindo.getText());
+        JLabel tempoAssistindo = hospedesTempoAssistindo.get(contaHospedes-1);
+        tempoAssistindo.setText(campoTempoAssistindo.getText());
+        JLabel id = hospedesId.get(contaHospedes-1);
+        id.setText(campoId.getText());
+    }
+
+}
